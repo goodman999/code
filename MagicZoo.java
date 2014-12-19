d6938eb85e9b2dc23702e87c48e80811ad56efa0
goodman999 <goodman999@users.noreply.github.com>
2014-12-19 14:25:08 +0100

    Update MagicZoo.java

--- MagicZoo.java
+++ MagicZoo.java
@@ -2,15 +2,17 @@ package magiczoo;
 
 public class MagicZoo {
 
-    //input zoo
-    static long z[] = { 17, 55, 6 };
+    //HIER KOMT INPUT !!!
+    //bijvoorbeeld: static long z[] = { 17, 55, 6 };
+    static long z[] = { 2055, 2006, 2017 };
+    
     //output end stand
     static long e[] = { 0, 0, 0 };
 
     public static void main(String[] args) {
 
         System.out.println("IN: ( " + z[0] + "," + z[1] + "," + z[2] + " )");
-        int overblijft, min; // index naar positie in z[]
+        int overblijft, min; // index naar positie in z[]  0,1,2
         
         if (((z[0] - z[1]) % 2 == 0) && ((z[1] - z[2]) % 2 == 0)) { 
             overblijft = getMax();
